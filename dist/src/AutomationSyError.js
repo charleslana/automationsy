32440d8c2a90f1dc03a988ed225b03fc16208fa2
Charles <63615970+charleslana@users.noreply.github.com>
2022-12-06 18:58:05 -0300

    Delete dist directory

--- dist/src/AutomationSyError.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class AutomationSyError extends Error {
-    constructor(message) {
-        super(message);
-        this.name = 'AutomationSyError';
-    }
-}
-exports.default = AutomationSyError;
